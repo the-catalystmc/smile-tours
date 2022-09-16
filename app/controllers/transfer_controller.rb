755ecb04e13e6e4cb9701196fb6176d417662da3
Rhyine <rhyinestewart@gmail.com>
2022-09-16 12:17:04 -0500

    Add transfer resources and routes

---
+++ app/controllers/transfer_controller.rb
@@ -0,0 +1,6 @@
+class TransferController < ApplicationController
+ 
+    def index
+    end
+  
+  end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'transfer', to: 'transfer#index', as: :transfer
+  get 'excursion', to: 'excursion#index', as: :excursion
   root 'home#index'
+
 end
