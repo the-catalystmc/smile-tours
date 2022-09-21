11caf6e9765292017b1ac7c6f3d1b39a0acea3ef
Rhyine <rhyinestewart@gmail.com>
2022-09-21 10:22:19 -0500

    Setup taxi rates page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'transfer', to: 'transfer#index', as: :transfer
   get 'excursion', to: 'excursion#index', as: :excursion
+  get 'taxi-rates', to: 'taxi#index', as: :taxi
   root 'home#index'
-
 end
